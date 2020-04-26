00d19b193eeb3091466a0b9967811eb887c3ccd6
esp32wrangler <44122875+esp32wrangler@users.noreply.github.com>
2020-04-26 19:50:16 +0200

    Update model.py

--- model.py
+++ model.py
@@ -12,6 +12,7 @@ import os
 #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
+#different data set experiments
 csvfile1 = open ("recordings/one/driving_log.csv", "r")
 csvfile2 = open ("recordings/two/driving_log.csv", "r")
 csvfile3 = open ("recordings/three/driving_log.csv", "r")
@@ -29,12 +30,16 @@ sidecorrection = 0.18
 
 X_train = []
 y_train = []
+#final dataset combining the goldilocks combination of experiments above
 lines = csvfile1.readlines() + csvfile2.readlines() + csvfile3.readlines() + csvfile4.readlines()  + \
-        csvfile5.readlines()  + csvfile6.readlines()  + csvfile7.readlines() + csvfile12.readlines() #+ csvfile10.readlines() #+ csvfile11.readlines()
-#csvfile10.readlines()        csvfile10.readlines() + csvfile10.readlines() + csvfile10.readlines()
+        csvfile5.readlines()  + csvfile6.readlines()  + csvfile7.readlines() + csvfile12.readlines()
 
+#split dataset to validation and training samples
 train_samples, valid_samples = train_test_split(lines, test_size=0.2)
 
+#read and preprocess images in batches
+#6 images generated: center original, center flipped, right camera view original and flipped, left camera and flipped
+# off-center images associated with a +-0.18 degree steering angle to simulate returning to centerline
 def generator(lines, batch_size=32):
     num_samples = len(lines)
     while 1:
@@ -74,10 +79,13 @@ train_generator = generator(train_samples, batch_size=batch_size)
 valid_generator = generator(valid_samples, batch_size=batch_size)
 
 model = keras.models.Sequential ()
+# normalize pixel values to -1..1
 model.add(keras.layers.Lambda(lambda x: (x-128)/128, input_shape=(160,320,3)))
+# crop top of image (typically it shows trees, sky and other irrelevant details, but on the second track this
+#   also means sacrificing some of the road ahead when there is a steep upward slope)
 model.add(keras.layers.Cropping2D(cropping=( (45, 25), (0, 0))))
 
-#model.add(keras.layers.Lambda(lambda x: (x-128)/128, input_shape=(160,320,3)))
+# Modified LeNet architecture with more channels and an additional convolutional layer
 model.add(keras.layers.Conv2D(12, (5,5), activation="relu"   ))
 model.add(keras.layers.MaxPooling2D())
 model.add(keras.layers.Conv2D(24, (5,5), activation="relu" ))
@@ -89,7 +97,6 @@ model.add(keras.layers.Dropout(0.3))
 model.add(keras.layers.Dense (120, activation="relu" ))
 model.add(keras.layers.Dropout(0.3))
 model.add(keras.layers.Dense (84, activation="relu" ))
-#model.add(keras.layers.Dropout(0.3))
 model.add(keras.layers.Dense (1))
 
 model.compile(optimizer='adam', loss='mse')
@@ -99,8 +106,10 @@ history_object = model.fit_generator(train_generator, validation_data=valid_gene
                                      validation_steps=(len(valid_samples)/batch_size),
                                      epochs=3, shuffle=True, verbose=1  )
 
+# save the model so the drive.py module can load it
 model.save('model.h5')
 
+#plot an epoch-by-epoch overview of the mean sq loss for training and validation data
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
 plt.ylabel ('mean sq loss')
